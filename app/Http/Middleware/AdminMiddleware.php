766a0d6641318122daef6bcbfc4ba29aae5b870d
saurav itachi <sauravkumar52778@gmail.com>
2024-11-10 22:20:39 -0800

    user and admin authetication done

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (Auth::check() && Auth::user()->isadmin) {
+            return $next($request);
+        }
+        redirect("/");
+
+        // Auth::logout();
+        // return redirect()->route('login')->withErrors(['email' => 'Admin only']);
+    }
+}
